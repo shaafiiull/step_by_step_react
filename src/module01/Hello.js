5e551792a1818ed08cbda40fd5fb045034ff3606
shaafiiull <thisisshaafii@gmail.com>
2023-03-03 12:19:55 +0600

    in this first module i wrote a functional component.

---
+++ src/module01/Hello.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+/* const Hello = () => {
+  return (
+    <div>
+      <h1>Hello everyone from react functional component</h1>
+    </div>
+  );
+}; */
+
+function Hello() {
+  return (
+    <div>
+      <h1>Hello everyone from react functional component</h1>
+    </div>
+  );
+}
+
+export default Hello;
